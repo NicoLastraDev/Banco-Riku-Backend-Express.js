975d4c161c8b88ab35bd12cf60b82a3578f06e48
Riku Okawa <nicolas.lastra.olguin@gmail.com>
2025-11-13 20:31:14 -0300

    UwU

--- src/config/db.js
+++ src/config/db.js
@@ -5,12 +5,21 @@ dotenv.config()
 
 const {Pool} = pkg
 
+// const pool = new Pool({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+// })
+
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  user: process.env.DB_USER || process.env.PGUSER,
+  host: process.env.DB_HOST || process.env.PGHOST,
+  database: process.env.DB_NAME || process.env.PGDATABASE,
+  password: process.env.DB_PASSWORD || process.env.PGPASSWORD,
+  port: process.env.DB_PORT || process.env.PGPORT,
+  ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
 })
 
 export default pool
